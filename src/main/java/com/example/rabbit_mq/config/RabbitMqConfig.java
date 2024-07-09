17a22510013f81f45f7a95a8d6ae028696c6e72a
kimD0ngjun <chickentasty0112@gmail.com>
2024-07-09 13:01:55 +0900

    feat: rabbitMQ 초기 설정 클래스 빈 생성

---
+++ src/main/java/com/example/rabbit_mq/config/RabbitMqConfig.java
@@ -0,0 +1,49 @@
+package com.example.rabbit_mq.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfig {
+
+    @Value("${rabbitmq.queue.name}")
+    private String queue;
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.binding.key}")
+    private String bindingKey;
+
+    // spring bean for rabbitmq queue
+    // Queue of spring AMQP
+    @Bean
+    public Queue queue() {
+        // queue name
+        return new Queue(queue);
+    }
+
+    // spring bean for rabbitmq exchange
+    @Bean
+    public TopicExchange exchange() {
+        // exchange name
+        return new TopicExchange(exchange);
+    }
+
+    // spring bean for rabbitmq binding
+    // bind between queue and exchange using routing key
+    @Bean
+    public Binding binding() {
+        return BindingBuilder
+                .bind(queue())
+                .to(exchange())
+                .with(bindingKey);
+    }
+
+    // ConnectionFactory, RabbitTemplate, RabbitAdmin 등은 이미 auto configuring 됨
+}
